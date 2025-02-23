9a4eedf86f4a4aa91e741cfbcf6973f336e541e2
Andrew Rowley <andrew.rowley160@outlook.com>
2025-02-23 11:51:50 -0500

    Create Main class and add dictionary

---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'yaml'
+
+require_relative 'lib/input'
+require_relative 'lib/game'
+
+class Main
+  # include Input
+
+  def self.run_hangman
+    puts '--- Hangman ---'
+    if Input.confirmation?('Load from save file? [y/n]') then
+      Main.load_game
+    else
+      Game.new.new_turn
+    end
+  end
+
+  def self.load_game
+    save_data = File.read('saved-games/saved_game.hangman')
+    saved_game = Marshal.load(save_data)
+    saved_game.new_turn
+  end
+end
+
+Main.run_hangman
